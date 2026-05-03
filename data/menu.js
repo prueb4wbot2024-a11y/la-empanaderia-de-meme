f8f292a34c7ac52b5553a9a784d98e6196a8be88
prueb4wbot2024-a11y <261629555+prueb4wbot2024-a11y@users.noreply.github.com>
2026-05-03 13:28:03 -0600

    Add menu data for food items

---
+++ data/menu.js
@@ -0,0 +1,7 @@
+[
+  {"id": 1, "nombre": "Empanadas de Carne Molida", "precio": 16},
+  {"id": 2, "nombre": "Empanadas de Pollo", "precio": 20},
+  {"id": 3, "nombre": "Guarache (Asado, Relleno Negro, Pollo, Molida)", "precio": 35},
+  {"id": 4, "nombre": "Salbutes Carne + Queso", "precio": 25},
+  {"id": 5, "nombre": "Tortas (Asado, Relleno Negro, Molida, Pollo)", "precio": 35}
+]
